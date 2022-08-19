fa1f0e983a040c9285bb0b1763a72228b9a2fa65
AbdElrahman-Taha-99 <55148764+AbdElrahman-Taha-99@users.noreply.github.com>
2022-08-19 15:55:18 +0200

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,44 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+rps = [rock, paper, scissors]
+
+choice = int(input("What do you choose: 0-->Rock, 1-->paper, 2-->scissors \n"))
+print(f"{rps[choice]}")
+
+print("Computer choice:")
+computer_choice = random.randint(0,2)
+print(f"{rps[computer_choice]}")
+
+if ((choice == 0)and(computer_choice == 2)) or ((choice == 1)and(computer_choice == 0)) or ((choice == 2)and(computer_choice == 1)):
+  print("You Win!")
+elif (choice == computer_choice):
+  print("Tie!")
+else:
+  print("You lost!")
